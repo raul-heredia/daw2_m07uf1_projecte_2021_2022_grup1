11bf8fdc1ea170b6170bd537c293bec89f23caed
macasa <15585369.clot@fje.edu>
2021-12-09 18:04:09 +0100

    Consulta de dades personals client

--- project/client.php
+++ project/client.php
@@ -4,39 +4,41 @@ if (isset($_SESSION['usuari'])) {
     $USERNAME = $_SESSION['usuari'][0];
     $USER = $_SESSION['usuari'][1];
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="./css/style.css" />
-<title>Document</title>
-<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-</head>
-<body>
-<nav>
-    <ul>
-        <li><a href=""><i class="fas fa-power-off"></i></a></li>
-        <li><a class="disabled"><i class="fas fa-user"></i><?php echo $USERNAME?></a></li>
-        <li><a href="">Enrere</a></li>
-    </ul>
-    </nav>
-    <main>
-        <h1>Benvingut <strong><?php echo $USER?></strong></h1>
-        <div class="options-flex">
-            <div class="option">
-                <i class="option-icon fas fa-user"></i>
-                <p class="option-p">Les meves dades</p>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css" />
+    <title>Document</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    </head>
+    <body>
+    <nav>
+        <ul>
+            <li><a href=""><i class="fas fa-power-off"></i></a></li>
+            <li><a class="disabled"><i class="fas fa-user"></i><?php echo $USERNAME?></a></li>
+            <li><a href="">Enrere</a></li>
+        </ul>
+        </nav>
+        <main>
+            <h1>Benvingut <strong><?php echo $USER?></strong></h1>
+            <div class="options-flex">
+                <a href="./scripts/llistar/clientD.php">
+                <div class="option">
+                    <i class="option-icon fas fa-user"></i>
+                    <p class="option-p">Les meves dades</p>
+                </div>
+                </a>
+                <div class="option">
+                    <i class="option-icon fas fa-book"></i>
+                    <p class="option-p">Llibres Disponibles</p>
+                </div>
             </div>
-            <div class="option">
-                <i class="option-icon fas fa-book"></i>
-                <p class="option-p">Llibres Disponibles</p>
-            </div>
-        </div>
-    </main>
-</body>
-</html>
+        </main>
+    </body>
+    </html>
 <?php
 }else{
     /* header("Location: ../../403.html"); */
---
+++ project/scripts/llistar/clientD.php
@@ -0,0 +1,71 @@
+<?php
+    session_start(); 
+    if (isset($_SESSION['usuari'])) {
+        $USERNAME = $_SESSION['usuari'][0];
+        $USER = $_SESSION['usuari'][1];
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="/var/www/html/css/style.css" />
+            <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+                integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+            <title>Les meves dades</title>
+            <style>
+        <?php 
+        include '/var/www/html/css/style.css'; // Per a que dompdf carregui el css correctament
+        ?>
+    </style>
+        </head>
+
+        <body>
+            <nav>
+            <ul>
+                <li><a href=""><i class="fas fa-power-off"></i></a></li>
+                <li><a class="disabled"><i class="fas fa-user"></i><?php echo $USERNAME?></a></li>
+                <li><a href="">Enrere</a></li>
+            </ul>
+            </nav>
+            <main>
+            <div class="options-flex">
+                <div class="option-list">
+            <?php
+            if (($CLIENTS = fopen("../../files/clients.csv", "r")) !== FALSE) {
+                while (($USUARIS = fgetcsv($CLIENTS, 1000, ",")) !== FALSE) {
+                    if ($USERNAME == $USUARIS[0]){
+                        ?>
+                        <ul>
+                            <li><strong>Nom Complet: </strong><?php echo $USER ?></li>
+                            <li><strong>Direcció: </strong><?php echo $USUARIS[4] ?></li>
+                            <li><strong>Direcció de Correu Electrònic: </strong><?php echo $USUARIS[5] ?></li>
+                            <li><strong>Nª de telèfon: </strong><?php echo $USUARIS[6] ?></li>
+                            
+                            <?php
+                                    if($USUARIS[7] == "true"){
+                                        if (($LLIBRES = fopen("../../files/llibres.csv", "r")) !== FALSE) {
+                                            while (($LLIBRE = fgetcsv($LLIBRES, 1000, ",")) !== FALSE) {
+                                                if($LLIBRE[0] == $USUARIS[8]){
+                                                    ?>
+                                                    <li><strong>ISBN Llibre en prèstec: </strong><?php echo $LLIBRE[0] ?></li> 
+                                                    <li><strong>Nom del llibre en prèstec: </strong><?php echo $LLIBRE[1] ?></li>
+                                                    <li><strong>Data d'inici prèstec: </strong><?php echo $USUARIS[9] ?></li> 
+                                                    <?php
+                                                }
+                                            }fclose($LLIBRES);
+                                            ?>
+                                    <li><strong></strong></li>
+                                    <?php
+                            }
+                        }
+                    }
+                }fclose($CLIENTS);
+            }
+            ?></div></div><?php
+    }else{
+        echo "error";
+    }
+?>
